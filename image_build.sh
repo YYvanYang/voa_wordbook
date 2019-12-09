269964db57f70779258b1310969c29cd39a4c631
Yvan Yang <yvan.yang@ulsee.com>
2019-12-09 15:40:01 +0800

    update image build

--- image_build.sh
+++ image_build.sh
@@ -1,5 +1,17 @@
 #!/bin/bash
 
+# Check if .env file exists
+if [ -e .env ]; then
+    source .env
+else
+    echo "Please set up your .env file before building your docker image."
+    exit 1
+fi
+
+# https://stackoverflow.com/questions/19331497/set-environment-variables-from-file-of-key-pair-values
+unset $(grep -v '^#' .env | sed -E 's/(.*)=.*/\1/' | xargs)
+export $(grep -v '^#' .env | xargs)
+
 docker build -t voa_wordbook .
 
 echo "Remove unused images."
