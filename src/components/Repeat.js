b65ef13ca6233129949a13cc4de84e7e8ddcd2ef
Yvan Yang <yvan.yang@ulsee.com>
2019-12-13 15:23:37 +0800

    fixed toolitp

--- src/components/Repeat.js
+++ src/components/Repeat.js
@@ -5,10 +5,17 @@ import IconButton from "@material-ui/core/IconButton"
 import RepeatOneIcon from "@material-ui/icons/RepeatOne"
 import RepeatIcon from "@material-ui/icons/Repeat"
 import SyncDisabledIcon from "@material-ui/icons/SyncDisabled"
+import Tooltip from "@material-ui/core/Tooltip"
 
 const Repeat = ({ repeatType, repeat }) => {
+  let component = (
+    <IconButton color="inherit" aria-label="Disable Repeat" onClick={repeat}>
+      <SyncDisabledIcon />
+    </IconButton>
+  )
+
   if (repeatType === "one") {
-    return (
+    component = (
       <IconButton color="inherit" aria-label="Repeat One" onClick={repeat}>
         <RepeatOneIcon />
       </IconButton>
@@ -16,18 +23,14 @@ const Repeat = ({ repeatType, repeat }) => {
   }
 
   if (repeatType === "all") {
-    return (
+    component = (
       <IconButton color="inherit" aria-label="Repeat" onClick={repeat}>
         <RepeatIcon />
       </IconButton>
     )
   }
 
-  return (
-    <IconButton color="inherit" aria-label="Disable Repeat" onClick={repeat}>
-      <SyncDisabledIcon />
-    </IconButton>
-  )
+  return <Tooltip title="Repeat Setting">{component}</Tooltip>
 }
 
 const mapStateToProps = ({ repeatType }) => {
